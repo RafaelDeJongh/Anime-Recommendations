f44064743455cc85514d2d076bf0d63d64885fff
Rafael De Jongh <rafael_boba_fett@msn.com>
2018-11-06 15:15:56 +0100

    Updated Script

--- js/index.js
+++ js/index.js
@@ -1,17 +1,30 @@
-let animeCon ="https://data.rafaeldejongh.com/data/sites/AnimeRecommendations/data/anime.json";
+let animeCon = "https://data.rafaeldejongh.com/data/sites/AnimeRecommendations/data/anime.json";
 let jikanAPI = "https://api.jikan.moe/v3/search/anime?q=";
-fetch(animeCon).then(response => response.json()).then((animeRecs)=>{
-	document.querySelector("header").innerHTML = "<h2>"+animeRecs.Personal.Section+"</h2>";
-		for(var i=0;i<=animeRecs.Personal.Top.length;i++){
-			fetch(jikanAPI+animeRecs.Personal.Top[i].animeTitle).then(res=>res.json()).then((data)=>{
+let animeMD = "https://raw.githubusercontent.com/RafaelDeJongh/Anime-Recommendations/master/data/anime.md";
+/*fetch(animeCon).then(response => response.json()).then((animeRecs)=>{
+	document.querySelector("header").innerHTML = "<h2>"+animeRecs.Action.Section+"</h2>";
+		for(var i=0;i<=animeRecs.Action.Anime.length;i++){
+			fetch(jikanAPI+animeRecs.Action.Anime[i].animeTitle).then(res=>res.json()).then((data)=>{
 					var data = data.results[0],
 							animeTitle = data.title,
 							animeURL = data.url,
 							animeSRC = data.image_url,
-							//animeIMG = animeSRC.replace(/(\.[^.]*)$/i,'l$1');
 							animeIMG = animeSRC.substring(0,animeSRC.lastIndexOf("."))+"l"+animeSRC.substring(animeSRC.lastIndexOf("."));
 					document.querySelector("main").innerHTML += '<a href="'+animeURL+'" target="_blank"><figure><img src="'+animeIMG+'" alt="'+animeTitle+'"><figcaption>'+animeTitle+'</figcaption></figure></a>';
-			console.log(data);
-			}).catch(err=>{ throw err});
+			}).catch(err=>{throw err});
+		}
+}).catch(err=>{throw err});*/
+
+fetch(animeMD).then(res=>res.text()).then((data)=>{
+	var animeTitles = data.match(/(?<=- ).*|(?<=\d\. ).*/g);
+		for(var i=0;i<animeTitles.length;i++){
+				fetch(jikanAPI+animeTitles[i]).then(res=>res.json()).then((data)=>{
+					var data = data.results[0],
+							animeTitle = data.title,
+							animeURL = data.url,
+							animeSRC = data.image_url,
+							animeIMG = animeSRC.substring(0,animeSRC.lastIndexOf("."))+"l"+animeSRC.substring(animeSRC.lastIndexOf("."));
+					document.querySelector("main").innerHTML += '<a href="'+animeURL+'" target="_blank"><figure><img src="'+animeIMG+'" alt="'+animeTitle+'"><figcaption>'+animeTitle+'</figcaption></figure></a>';
+			}).catch(err=>{throw err});
 		}
 }).catch(err=>{throw err});
